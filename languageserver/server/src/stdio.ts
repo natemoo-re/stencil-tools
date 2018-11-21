8000159ac9c9dce53c8496a071a39656446beb13
natemoo-re <ntmoore@knox.edu>
2018-11-21 18:16:34 -0500

    feat(): add support for ipc or stdio

---
+++ languageserver/server/src/stdio.ts
@@ -0,0 +1,5 @@
+import { StencilLanguageServer } from './language-server';
+import { createConnection, IConnection, StreamMessageReader, StreamMessageWriter } from 'vscode-languageserver';
+
+const connection: IConnection = createConnection(new StreamMessageReader(process.stdin), new StreamMessageWriter(process.stdout));
+new StencilLanguageServer(connection);
