99d3b4f9117c8ac4596742098b0068d031c5193d
natemoo-re <ntmoore@knox.edu>
2018-11-21 19:16:04 -0500

    chore(): update to ipc module

--- extensions/vscode/src/utils/server.ts
+++ extensions/vscode/src/utils/server.ts
@@ -4,7 +4,7 @@ import { ServerOptions, TransportKind } from 'vscode-languageclient';
 
 export const getServerOptions = (context: ExtensionContext): ServerOptions => {
 
-    const serverModule = context.asAbsolutePath(path.join('node_modules', '@stencil-tools', 'languageserver', 'dist', 'main.js'));
+    const serverModule = context.asAbsolutePath(path.join('node_modules', '@stencil-tools', 'languageserver', 'dist', 'ipc.js'));
 
     const run = { module: serverModule, transport: TransportKind.ipc };
     const debugOptions = { execArgv: ["--inspect=6009"] };
